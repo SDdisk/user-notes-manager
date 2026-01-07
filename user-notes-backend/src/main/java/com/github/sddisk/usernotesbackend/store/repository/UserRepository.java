e9599d70b667cdd022779a1c4f5f06c67d622d69
Artem Rynochnov <TemiX002@mail.ru>
2026-01-07 13:03:31 +0400

    Add user repository

---
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/store/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.github.sddisk.usernotesbackend.store.repository;
+
+import com.github.sddisk.usernotesbackend.store.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
