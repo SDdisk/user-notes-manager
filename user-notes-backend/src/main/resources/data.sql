8649bbde1184a078c995b2b57ca792d805e4c3f8
Artem Rynochnov <TemiX002@mail.ru>
2026-01-07 15:29:25 +0400

    Add data.sql with initial user data seeding

---
+++ user-notes-backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_table(id, username, email, password)
+values ('550e8400-e29b-41d4-a716-446655440000', 'user', 'user@mail.com', 'password'),
+      ('550e8400-e29b-41d4-a716-446655440001', 'admin', 'admin@mail.com', 'admin');
