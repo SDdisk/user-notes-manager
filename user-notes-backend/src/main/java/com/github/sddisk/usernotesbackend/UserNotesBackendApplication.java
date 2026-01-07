6166877864425a79342407f2adc92a50971f75fc
Artem Rynochnov <TemiX002@mail.ru>
2026-01-07 10:57:37 +0400

    Add new module user-notes-backend

---
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/UserNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.sddisk.usernotesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNotesBackendApplication.class, args);
+    }
+
+}
