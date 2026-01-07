8d149abc8a86eaac1b0e591ae291e9e323d4a4d5
Artem Rynochnov <TemiX002@mail.ru>
2026-01-07 11:09:54 +0400

    Add h2 dependency

--- user-notes-backend/build.gradle.kts
+++ user-notes-backend/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
