93cb04e044d66a4f25cb60c5790f34a4ec2ebe79
Artem Rynochnov <TemiX002@mail.ru>
2026-01-08 11:59:59 +0400

    Add user details with service implementation

---
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/security/UserDetailsAdapter.java
@@ -0,0 +1,34 @@
+package com.github.sddisk.usernotesbackend.security;
+
+import com.github.sddisk.usernotesbackend.store.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public class UserDetailsAdapter implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase(Locale.ENGLISH)))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
