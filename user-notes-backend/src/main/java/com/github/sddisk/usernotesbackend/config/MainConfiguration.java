ea5f9eac5939a177fd982e4b01f08fc3e13cc2f7
Artem Rynochnov <TemiX002@mail.ru>
2026-01-10 11:59:52 +0400

    Add main cofiguration and refactor jwt configuration properties

--- user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/config/MainConfiguration.java
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/config/MainConfiguration.java
@@ -1,4 +1,13 @@
 package com.github.sddisk.usernotesbackend.config;
 
+import com.github.sddisk.usernotesbackend.jwt.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(
+        value = { JwtProperties.class }
+)
 public class MainConfiguration {
+
 }
--- user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/jwt/JwtProperties.java
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/jwt/JwtProperties.java
@@ -3,10 +3,8 @@ package com.github.sddisk.usernotesbackend.jwt;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties("jwt")
+@ConfigurationProperties(prefix = "jwt")
 @Getter @Setter
 public class JwtProperties {
     private String secretKey;
