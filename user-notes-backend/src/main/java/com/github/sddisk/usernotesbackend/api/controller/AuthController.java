2a4b5454f32115ace782eed7044549ef7ef29b72
Artem Rynochnov <TemiX002@mail.ru>
2026-01-11 20:55:24 +0400

    Add authentication controller

---
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/api/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.github.sddisk.usernotesbackend.api.controller;
+
+import com.github.sddisk.usernotesbackend.api.dto.auth.AuthResponse;
+import com.github.sddisk.usernotesbackend.api.dto.auth.LoginRequestDto;
+import com.github.sddisk.usernotesbackend.api.dto.auth.RegisterRequestDto;
+import com.github.sddisk.usernotesbackend.service.auth.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse register(@RequestBody RegisterRequestDto registerRequestDto, HttpServletResponse response) {
+        return authService.register(registerRequestDto, response);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        return authService.login(loginRequestDto, response);
+    }
+
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(HttpServletResponse response) {
+        authService.logout(response);
+    }
+
+    @PostMapping("/refresh-token")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse refreshToken(@CookieValue("refreshToken") String refreshToken) {
+        return authService.refreshToken(refreshToken);
+    }
+
+}
