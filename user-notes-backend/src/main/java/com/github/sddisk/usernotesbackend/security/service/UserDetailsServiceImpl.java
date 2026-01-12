873ec6b921922a226beafa2cc107c7dbdc62ff78
Artem Rynochnov <TemiX002@mail.ru>
2026-01-12 12:14:49 +0400

    Fix log

--- user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/security/service/UserDetailsServiceImpl.java
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/security/service/UserDetailsServiceImpl.java
@@ -17,7 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username" + username + " not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
         return new UserDetailsAdapter(user);
     }
 }
