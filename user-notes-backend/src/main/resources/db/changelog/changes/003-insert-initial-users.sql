7e2a7f3e85f3f8a291461da20d20664439633153
Artem Rynochnov <TemiX002@mail.ru>
2026-01-18 09:56:45 +0400

    Migrate initial users data from data.sql to Liquibase changelog

---
+++ user-notes-backend/src/main/resources/db/changelog/changes/003-insert-initial-users.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset artem:003-insert-initial-users
+insert into user_table(user_id, username, email, password, roles)
+values ('550e8400-e29b-41d4-a716-446655440000', 'user', 'user@mail.com', '$2a$10$hmeBSV.B7I06r8FhWOPwN.6JNIk7ZnbihqOkb3qCituMSJaWqtSFK', ARRAY['USER']),
+       ('550e8400-e29b-41d4-a716-446655440001', 'admin', 'admin@mail.com', '$2a$10$50e3syPci07I6zoMfAj5KORQ.GeACbEPgvV7BB3Y87miQFuJnwE.K', ARRAY['ADMIN', 'USER']);
