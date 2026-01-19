9021799ba1ff841638859bdcefa02c4ea559cba5
Artem Rynochnov <TemiX002@mail.ru>
2026-01-19 16:55:44 +0400

    Update RegisterRequestDto

--- user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/api/dto/auth/RegisterRequestDto.java
+++ user-notes-backend/src/main/java/com/github/sddisk/usernotesbackend/api/dto/auth/RegisterRequestDto.java
@@ -13,7 +13,6 @@ public record RegisterRequestDto(
         @Email(message = "Incorrect email format")
         String email,
 
-        @Size(min = 9, message = "Password length must be 9 characters or bigger")
         @Pattern(
                 regexp = "^(?=(.*\\d){5})(?=(.*[a-zA-Z]){3})(?=(.*[@#$%^&+=!]){1})[a-zA-Z\\d@#$%^&+=!]{9}$",
                 message = "Password must contains: 5 digits, 3 letters and 1 special character"
