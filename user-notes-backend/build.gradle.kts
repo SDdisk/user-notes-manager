634fe388667ce3eaba359c4aa538601c2d0026a3
Artem Rynochnov <TemiX002@mail.ru>
2026-01-21 20:12:07 +0400

    Refactor dependencies

--- user-notes-backend/build.gradle.kts
+++ user-notes-backend/build.gradle.kts
@@ -25,27 +25,45 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    // web
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    // jwt
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
     implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // db migration
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-//    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
-//    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:postgresql")
-    compileOnly("org.projectlombok:lombok")
+
+    // db drivers
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-h2console")
+
+    // devtools
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    runtimeOnly("com.h2database:h2")
-    implementation("org.springframework.boot:spring-boot-h2console")
-    runtimeOnly("org.postgresql:postgresql")
+    // testcontainers
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:postgresql:1.19.3")
+
+    // test runtime
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.withType<Test> {
